7a6261bc84f7b0dc098017e41521d085729527d2
jdiederich2 <jdiederich2@student.cvtc.edu>
2019-03-19 23:48:39 -0500

    Nav Menu

---
+++ includes/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Untitled Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    
+    <link href="css/nav.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+    
+    </header>
+
+    <nav>
+        <ul class="mainMenu">
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Dairy</a>
+                <div class="dropdown-content">
+                    <a href="#">Ptn</a>
+                    <a href="#">Calf Rations</a>
+                    <a href="#">Heard Builder</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Beef</a>
+                <div class="dropdown-content">
+                    <a href="#">Custom Mixes</a>
+                    <a href="#">Minerals</a>
+                    <a href="#">Smartlics</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Poultry / Gamebird</a>
+                <div class="dropdown-content">
+                    <a>Egg Crumbles</a>
+                    <a>Meatmaker</a>
+                    <a>Custom Mixes</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Pig</a>
+                <div class="dropdown-content">
+                    <a>Swine Mineral</a>
+                    <a>Custom Batches</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Equine</a>
+                <div class="dropdown-content">
+                    <a>Total</a>
+                    <a>Senior</a>
+                    <a>34</a>
+                    <a>Pellets &#40;Blaffa&#41;</a>
+                    <a>Alfalfa Cubes</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Pet</a>
+                <div class="dropdown-content">
+                    <a>Dog</a>
+                    <a>Cat</a>
+                    <a>Rabbit</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Wildgame / Bear Bait</a>
+                <div class="dropdown-content">
+                    <a>Deer Mineral</a>
+                    <a>Trophy Rock</a>
+                    <a>Monsterbuck Food Plots</a>
+                    <a>Deer Corn</a>
+                    <a>Bird Seed</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Goat / Sheep</a>
+                <div class="dropdown-content">
+                    <a>Sheep Mineral</a>
+                    <a>Custom Mixes</a>
+                    <a>Goat Concentrate</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Organic Feeds</a>
+                <div class="dropdown-content">
+                    <a>Crystal Creek</a>
+                    <a>Cashton Feeds</a>
+                </div>
+            </li>
+        </ul>
+        
+        
+    </nav>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include 'includes/header.php';
+?>
+
+    <main>
+        <div class="grid">
+            <div class="grid-item">Website1</div>
+            <div class="grid-item">Website2</div>
+            <div class="grid-item">Website3</div>
+            <div class="grid-item">Website4</div>
+            <div class="grid-item">Website5</div>
+            <div class="grid-item"></div>
+        </div>
+    </main>
+</body>
+</html>
